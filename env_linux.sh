d19b2550144ae9bdcba0aeed0386cee2262146c4
lixianjing <xianjimli@hotmail.com>
2019-12-07 16:41:12 -0800

    update scripts

--- env_linux.sh
+++ env_linux.sh
@@ -1,4 +1,4 @@
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_111.jdk/Contents/Home
+export JAVA_HOME=/work/tools/jdk-13.0.1
 export JAVA_JNI_PATH="$JAVA_HOME/include"
 export JAVA_JNI_OS_PATH="$JAVA_HOME/include/linux"
 
