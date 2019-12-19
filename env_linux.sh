9dfc0bc9ea6683b9015093fff300c73e41acbaa6
xianjimli <xianjimli@hotmail.com>
2019-12-20 07:56:21 +0800

    update scripts

--- env_linux.sh
+++ env_linux.sh
@@ -1,4 +1,7 @@
 export JAVA_HOME=/work/tools/jdk-13.0.1
 export JAVA_JNI_PATH="$JAVA_HOME/include"
 export JAVA_JNI_OS_PATH="$JAVA_HOME/include/linux"
+export JAVA_LIB_PATH="$JAVA_HOME/lib/server"
+export LD_LIBRARY_PATH="$JAVA_HOME/lib/server"
+
 
