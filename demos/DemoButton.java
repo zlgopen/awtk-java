aa7177cf6a11605b2c2a6eea1c43f76304eb6cd5
xianjimli <xianjimli@gmail.com>
2019-12-20 12:07:46 +0800

    format code

--- demos/DemoButton.java
+++ demos/DemoButton.java
@@ -1,48 +1,48 @@
 import awtk.*;
 
 class DemoButton {
- public static void init(String[] args) {
-   int w = 320;
-   int h = 480;
+  public static void init(String[] args) {
+    int w = 320;
+    int h = 480;
 
-   if(args.length > 0) {
-     w = Integer.parseInt(args[0]);
-   }
-   
-   if(args.length > 1) {
-     h = Integer.parseInt(args[1]);
-   }
+    if (args.length > 0) {
+      w = Integer.parseInt(args[0]);
+    }
 
-   AWTK.init(w, h, TAppType.SIMULATOR);
- }
+    if (args.length > 1) {
+      h = Integer.parseInt(args[1]);
+    }
 
- public static void createUI() {
-   TWidget win = TWindow.createDefault();
-   TWidget button = TButton.create(win, 10, 0, 0, 0);
-   
-   button.setName("quit");
-   button.setText("Quit");
-   button.setSelfLayout("default(x=c,y=m,w=200,h=40)");
+    AWTK.init(w, h, TAppType.SIMULATOR);
+  }
 
-   button.on(TEventType.CLICK, new TOnEvent() {
+  public static void createUI() {
+    TWidget win = TWindow.createDefault();
+    TWidget button = TButton.create(win, 10, 0, 0, 0);
+
+    button.setName("quit");
+    button.setText("Quit");
+    button.setSelfLayout("default(x=c,y=m,w=200,h=40)");
+
+    button.on(TEventType.CLICK, new TOnEvent() {
       @Override
       public TRet onEvent(TEvent evt) {
         System.out.println("OnClick");
         TGlobal.quit();
         return TRet.OK;
       }
-   }, 0);
- }
+    }, 0);
+  }
 
- public static void main(String[] args) {
-   DemoButton.init(args);
-   DemoButton.createUI();
+  public static void main(String[] args) {
+    DemoButton.init(args);
+    DemoButton.createUI();
 
-   AWTK.run();
- }
+    AWTK.run();
+  }
 
- static {
-  System.loadLibrary("awtk-jni");
- }
- 
+  static {
+    System.loadLibrary("awtk-jni");
+  }
+
 }
