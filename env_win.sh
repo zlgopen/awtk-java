f9fb595eacf6074657bd0ad3cafa4aa5592d2402
lixianjing <xianjimli@hotmail.com>
2019-12-29 19:14:11 +0800

    update scripts

--- env_win.sh
+++ env_win.sh
@@ -1,4 +1,5 @@
 export JAVA_HOME="/c/Program Files/Java/jdk1.8.0_231"
 export JAVA_JNI_PATH="$JAVA_HOME/include"
 export JAVA_JNI_OS_PATH="$JAVA_HOME/include/win32"
-
+export JAVA_LIB_PATH="$JAVA_HOME/lib"
+cp -fv "$JAVA_HOME/jre/bin/server/jvm.dll" .
