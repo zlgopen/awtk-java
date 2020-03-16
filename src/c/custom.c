d5afe0daab19f4451df290b587843831662e71ff
xianjimli <xianjimli@gmail.com>
2020-03-16 21:43:28 +0800

    sync assets

--- src/c/custom.c
+++ src/c/custom.c
@@ -1,11 +1,11 @@
 #include "awtk.h"
 
-extern ret_t assets_init(void);
+extern ret_t assets_init(const char* theme);
 
 JNIEXPORT jint JNICALL Java_awtk_AWTK_init(JNIEnv* env, jclass ajc, jint w, jint h,
                                            jint app_type) { /*func*/
   tk_init(w, h, APP_SIMULATOR, "AWTK-JAVA", NULL);
-  assets_init();
+  assets_init("default");
   tk_ext_widgets_init();
   system_info_set_default_font(system_info(), "default_full");
 
